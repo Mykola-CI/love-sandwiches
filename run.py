619daf6dab852c10b8982f62794b5eb52b77df4c
Mykola Kuievda <dankraig23@gmail.com>
2024-01-05 11:33:44 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here:\n")
         sales_data = data_str.split(",")
         
         if validate_data(sales_data):
